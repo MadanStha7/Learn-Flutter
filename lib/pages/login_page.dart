7b6468b9b6abdb36577761f44cf6dfb42c67e268
MadanStha7 <madanstha.124@gmail.com>
2022-05-24 16:05:24 +0545

    Adding Images and Login form

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,19 +1,64 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+    return Material(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login.png",
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 20.0,
+                // child: Text("Madan"),
+              ),
+              const Text(
+                " Welcome",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: 'Username',
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter password",
+                      labelText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hi Madan");
+                    },
+                    child: const Text("Login"),
+                    style: TextButton.styleFrom(minimumSize: const Size(100, 40)),
+                  )
+                ]),
+              )
+            ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
